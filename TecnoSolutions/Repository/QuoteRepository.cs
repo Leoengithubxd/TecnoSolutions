b1d03eb578412291c1330e7e27b102e6656788d4
leoengithubxd <gameractive90@gmail.com>
2025-02-20 01:04:53 -0500

    Asignacion de Datos de Cotizacion

--- TecnoSolutions/Repository/QuoteRepository.cs
+++ TecnoSolutions/Repository/QuoteRepository.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using TechnoSolutions.Dtos;
+using TecnoSolutions.Dtos;
 using TecnoSolutions.Models;
 namespace TechnoSolutions.Repositories
 {
@@ -10,7 +13,7 @@ namespace TechnoSolutions.Repositories
     {
         public List<ServiceSelectionDto> GetAllServices() //Traer tabla Service
         {
-            using (var db = new BD_14_02Entities4())
+            using (var db = new BD_14_02Entities())
             {
                 var services = db.SERVICE
                     .Select(p => new
@@ -30,11 +33,29 @@ namespace TechnoSolutions.Repositories
             }
         }
 
+        public void UpdateQuotes(List<QuoteDto> quotes)
+        {
+            using (var connection = new SqlConnection("Data Source=LEO; Initial Catalog=BD 14_02; Integrated Security=true"))
+            {
+                connection.Open();
+                foreach (var quote in quotes)
+                {
+                    var query = @"
+                UPDATE QUOTE 
+                SET IdCrewPerson = @IdCrew, 
+                    StarDate = @StarDate, 
+                    EndDate = @EndDate, 
+                    Price = @Price 
+                WHERE IdQuote = @IdQuote";
+                    connection.Execute(query, quote);
+                }
+            }
+        }
         public void SaveSelectedServices(
             int userId, List<ServiceSelectionDto> selectedServices, 
             string address, string department, string city) //Cargar Cotizacion
         {
-            using (var db = new BD_14_02Entities4())
+            using (var db = new BD_14_02Entities())
             {
                 foreach (var service in selectedServices)
                 {
@@ -57,6 +78,24 @@ namespace TechnoSolutions.Repositories
                     db.QUOTE_SERVICE.Add(quoteService);
                 }
                 db.SaveChanges();
+            }
+        }
+        public List<QuoteDto> GetAllQuotes()
+        {
+            using (var db = new BD_14_02Entities())
+            {
+                var quotes = from q in db.QUOTE
+                             join p in db.PERSON on q.IdPerson equals p.IdPerson
+                             join s in db.STATE on q.IdState equals s.IdState
+                             select new QuoteDto
+                             {
+                                 IdQuote = q.IdQuote,
+                                 PersonName = p.FirstName + " " + p.LastName,
+                                 State = s.Name,
+                                 ServiceAddress = q.ServiceAddress,
+                             };
+
+                return quotes.ToList();
             }
         }
     }
